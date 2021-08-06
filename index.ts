fb5f991848266a5bd5ab873422bffe1cf3667b33
Omar ElGaml <omar@gloot.com>
2021-08-06 10:49:54 +0200

    Fixed typo in export

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import SingleEliminationBracket from './src/bracket-single/single-elim-bracket';
-import DoulbeEliminationBracket from './src/bracket-double/double-elim-bracket';
+import DoubleEliminationBracket from './src/bracket-double/double-elim-bracket';
 import Match from './src/components/match';
 import { MATCH_STATES } from './src/core/match-states';
 import SVGViewer from './src/svg-viewer';
 
 export {
   SingleEliminationBracket,
-  DoulbeEliminationBracket,
+  DoubleEliminationBracket,
   Match,
   MATCH_STATES,
   SVGViewer,
