0e99596bd4a86cb3ee9e6e147eefb58ca52c0e2c
Shenato <urreaped@gmail.com>
2021-12-19 15:15:43 +0100

    Added createTheme to exports

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import DoubleEliminationBracket from './src/bracket-double/double-elim-bracket';
 import Match from './src/components/match';
 import { MATCH_STATES } from './src/core/match-states';
 import SVGViewer from './src/svg-viewer';
+import { createTheme } from './src/themes/themes';
 
 export {
   SingleEliminationBracket,
@@ -10,4 +11,5 @@ export {
   Match,
   MATCH_STATES,
   SVGViewer,
+  createTheme,
 };
